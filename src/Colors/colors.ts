1c4ca5e48f1106d9444539d0644baeac1c61ae45
Filipe <filipelima849@gmail.com>
2023-05-18 13:35:41 -0300

    Adicionado arquivos de fontes e cores para usar no sistema

---
+++ src/Colors/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    gray100: "#f2f2f2",
+    gray200: "#d9d9d9",
+    gray300: "#333333",
+    gray400: "#262626",
+    gray500: "#808080",
+    gray700: "#0d0d0d",
+    blue: "#4EA8DE",
+    blueDark: "#1E6F9F",
+    purple: "#8284FA"    
+}
---
+++ src/Fonts/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts = {
+    interBold: "Inter_700Bold",
+    interRegular: "Inter_400Regular"
+}
