e4a9ad544f5231fac740d7ae272d70879e9cc05d
Filipe <filipelima849@gmail.com>
2023-05-18 15:22:55 -0300

    Adicionado componente na página Home

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -8,6 +8,7 @@ import { colors } from "../Colors/colors";
 import { styles } from "./styles";
 import { useFonts, Inter_700Bold, Inter_400Regular } from "@expo-google-fonts/inter";
 import AppLoading from "expo-app-loading";
+import { Tarefa } from "../Components/Tarefa";
 
 export default function Home() {
     let [fontsLoaded] = useFonts({
@@ -29,7 +30,7 @@ export default function Home() {
             <TextInput
                 style={styles.input}
                 placeholder="Adicione uma nova tarefa"
-                placeholderTextColor={colors.gray500}
+                placeholderTextColor={colors.gray300}
             />
             <TouchableOpacity style={styles.addButton}>
                 <Text>
@@ -48,6 +49,7 @@ export default function Home() {
             </View>
         </View>
         <View style={styles.hr} />
+        <Tarefa />
         <View style={styles.empty}>
             <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={100} color={colors.gray200} />
             <Text style={styles.textEmptyBold}>Você ainda não tem tarefas cadastradas</Text>
